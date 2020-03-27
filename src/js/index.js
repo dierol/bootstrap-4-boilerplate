66e33a47c24aa44c62211d211dc39cbcf60b6fe3
dierol <diegorolle90@gmail.com>
2020-03-27 13:03:13 +0100

    [WIP]

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 
+
+
+var i = 1;
+(function loop() {
+  var bgimage = new Image();
+  bgimage.src="../img/login"+i+".jpg";
+
+  $(bgimage).on('load', function(){
+      $(".login-image").css("background-image","url('"+$(this).attr("src")+"')").removeClass('invisible').addClass('animated fadeIn delay-1s');
+  });
+
+  if (3 === i) {
+    i = 1;
+  } else {
+    i++;
+  }
+  console.log(i);
+  setTimeout(loop, 3000);
+})();
